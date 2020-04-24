20b51114eef78e8b9f9c3fb33a5976e7fdcb95ef
Andrew Scagnelli <ascagnel@gmail.com>
2020-04-24 16:53:02 -0400

    cleanup, updating install file

--- install.sh
+++ install.sh
@@ -31,15 +31,17 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
     fi
 fi
 if [[ -z "$XDG_CONFIG_HOME" ]]; then
-    echo "\$XDG_CONFIG_HOME is not set; setting to \$HOME/Library/Preferences"
-    read -p "Press any key to continue... " -n1 -s
     if [[ "$OSTYPE" =~ ^darwin ]]; then
+        echo "\$XDG_CONFIG_HOME is not set; setting to \$HOME/Library/Preferences"
+        read -p "Press any key to continue... " -n1 -s
         export XDG_CONFIG_HOME=$HOME/Library/Preferences
     else
+        echo "\$XDG_CONFIG_HOME is not set; setting to \$HOME/.config"
+        read -p "Press any key to continue... " -n1 -s
         export XDG_CONFIG_HOME=$HOME/.config
     fi
 else
-    echo "XDG_CONFIG_HOME already set; not resetting"
+    echo "XDG_CONFIG_HOME already set to $XDG_CONFIG_HOME; not resetting"
 fi
 echo  '\n'
 if [ ! -d "$HOME/.vim" ]; then
@@ -54,58 +56,78 @@ if [ ! -d "$HOME/.vim" ]; then
 fi
 
 if [ ! -f "$HOME/.nvimrc" ]; then
-    ln -s $PWD/.nvimrc $HOME/.vimrc
+    ln -s $PWD/nvimrc $HOME/.vimrc
     echo "linked .vimrc"
 else
     echo "Not linking .vimrc"
 fi
 
 if [ ! -f "$HOME/.nvimrc" ]; then
-    ln -s $PWD/.nvimrc $HOME/.nvimrc
+    ln -s $PWD/nvimrc $HOME/.nvimrc
+    echo "Linked nvimrc file"
 else
     echo "Not linking .nvimrc"
 fi
 
 if [ ! -f "$HOME/.zshrc" ]; then
-    ln -s $PWD/.zshrc $HOME/.zshrc
-    echo "linked .zshrc"
+    ln -s $PWD/zshrc $HOME/.zshrc
+    echo "Linked .zshrc"
 else
     echo "Not linking .zshrc"
 fi
 
 if [ ! -f "$HOME/.zlogin" ]; then
-    ln -s $PWD/.zlogin $HOME/.zlogin
-    echo "linked .zlogin"
+    ln -s $PWD/zlogin $HOME/.zlogin
+    echo "Linked .zlogin"
 else
     echo "Not linking .zlogin"
 fi
 
+if [ ! -f "$HOME/.zlogout" ]; then
+    ln -s $PWD/zlogout $HOME/.zlogout
+    echo "Linked zlogout file"
+else
+    echo "Not linking zlogout"
+fi
+
+if [ ! -f "$HOME/.screenrc" ]; then
+    ln -s $PWD/screenrc $HOME/.screenrc
+    echo "Linked screenrc file"
+else
+    echo "Not linking screenrc"
+fi
+
 if [ ! -f "$HOME/.tmux.conf" ]; then
-    ln -s $PWD/.tmux.conf $HOME/.tmux.conf
-    echo "linked tmux config"
+    ln -s $PWD/tmux.conf $HOME/.tmux.conf
+    echo "Linked tmux config"
 else
     echo "Not linking tmux config"
 fi
 
 if [ ! -f "$HOME/.gitconfig" ]; then
-    ln -s $PWD/.gitconfig $HOME/.gitconfig
-    echo "linked git config"
+    ln -s $PWD/gitconfig $HOME/.gitconfig
+    echo "Linked git config"
 else
     echo "Not linking git config"
 fi
 
 if [ ! -f "$HOME/.gitignore_global" ]; then
-    ln -s $PWD/.gitignore_global $HOME/.gitignore_global
+    ln -s $PWD/gitignore_global $HOME/.gitignore_global
     echo "Linked gitignore"
 else
     echo "Not linking gitignore"
 fi
 
 if [ ! -d "$XDG_CONFIG_HOME/nvim" ]; then
-    echo Creating $XDG_CONFIG_HOME/nvim
     mkdir $XDG_CONFIG_HOME/nvim
-    ln $PWD/.nvimrc $XDG_CONFIG_HOME/nvim/init.vim
-    echo "Linked .nvimrc to XDG_CONFIG_HOME"
+    echo "Created \$XDG_CONFIG_HOME/nvim"
 else 
-    echo "Not linking .nvimrc to XDG_CONFIG_HOME"
+    echo "Not creating \$XDG_CONFIG_HOME/nvim"
+fi
+
+if [ ! -f "$XDG_CONFIG_HOME/nvim/init.vim" ]; then
+    ln $PWD/nvimrc $XDG_CONFIG_HOME/nvim/init.vim
+    echo "Linked .nvimrc to \$XDG_CONFIG_HOME"
+else 
+    echo "Not linking .nvimrc to \$XDG_CONFIG_HOME"
 fi
